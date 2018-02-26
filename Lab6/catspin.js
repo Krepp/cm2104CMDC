9936a15e9c1455726d2fb9df30f978e905325cb3
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-02-26 17:06:38 +0000

    BIG PUSH COMING IN WOOOOAAAAh

--- Lab6/catspin.js
+++ Lab6/catspin.js
@@ -6,12 +6,12 @@ var cat = document.querySelector("img");
 var angle = 0, lastTime = null;
 function animate(time) {
     if (lastTime != null)
-        angle += (time - lastTime) * 0.001;
+        angle += (time - lastTime) * 0.005;
     lastTime = time;
-    cat.style.top = (Math.sin(angle) * 20) + "px";
+    cat.style.top = (Math.sin(angle) * 200) + "px";
     cat.style.left = (Math.cos(angle) * 200) + "px";
+    cat.style.transform = "scale(" + Math.sin(angle*0.05) + "," + Math.sin(angle*0.05) +")";
+
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
-
-
