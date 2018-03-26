40ca328060d0dd9f59f504606f88ffbfcbf1ee64
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-03-26 16:48:12 +0100

    Hey ho LETS GO

--- Lab13/myFirstServer.js
+++ Lab13/myFirstServer.js
@@ -1,8 +1,11 @@
 var http = require('http');
-var currentdate = require('./myModule');
+var oneliner = require('one-liner-joke');
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + currentdate.myDateTime());
-  res.end('Hello World!');
+  // res.write("The date and time are currently: " + currentdate.myDateTime());
+  // res.end('Hello World!');
+  var randomJoke = oneliner();
+  res.end(randomJoke);
+
 }).listen(8080);
