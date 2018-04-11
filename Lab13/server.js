d967b8a0a38fac711c55f75e8fa1e8510d25a37b
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-04-11 13:05:59 +0100

    hdsfkhdsaf

--- Lab13/server.js
+++ Lab13/server.js
@@ -9,4 +9,11 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){
  res.send("this is route 2");
 });
+
+app.get('/getform', function(req, res){
+var name = req.query.name;
+var quest = req.query.quest;
+ res.send("Hi "+name+" I am sure you will "+quest) ;
+});
+
 app.listen(8080);
