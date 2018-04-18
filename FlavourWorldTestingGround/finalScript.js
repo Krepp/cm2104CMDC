8f810635fcadc8453bc84e84a559cd49641525ae
Peter Ramsbottom <PBottom@Peters-MacBook-Air.local>
2018-04-18 12:39:41 +0100

    Hey ho, lets go

--- FlavourWorldTestingGround/finalScript.js
+++ FlavourWorldTestingGround/finalScript.js
@@ -24,7 +24,7 @@ function printJSON(jsondata){
 
 function addResultTitles(jsondata){
   var htmlstring = "";
-  var length = Object.keys(jsonData).length;
+  var length = Object.keys(jsondata).length;
 
   for (var i = 0; i < length; i++){
     var title = jsondata.Search[i].recipeName;
