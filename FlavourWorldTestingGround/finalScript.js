30f0f35a35da0a9244d3d8818fd6f3e3bfeaf52d
Peter Ramsbottom <PBottom@Peters-MacBook-Air.local>
2018-04-18 12:34:24 +0100

    Displays only recipe names

--- FlavourWorldTestingGround/finalScript.js
+++ FlavourWorldTestingGround/finalScript.js
@@ -13,7 +13,7 @@ function getResultsFromOMDB(searchterms){
   var url = "https://api.yummly.com/v1/api/recipes?_app_id=b96a6669&_app_key=68fc92d94c14efafd327d91916587827&q=" + searchterms;
 
   $.getJSON(url, function(jsondata){
-    printJSON(jsondata);
+    addResultsTitles(jsondata);
   });
 }
 
@@ -24,9 +24,10 @@ function printJSON(jsondata){
 
 function addResultTitles(jsondata){
   var htmlstring = "";
+  var length = jsondata.length;
 
-  for (var i = 0; i < 10; i++){
-    var title = jsondata.Search[i].id;
+  for (var i = 0; i < length; i++){
+    var title = jsondata.Search[i].recipeName;
     htmlstring += "<li>" + title + "</li>";
   }
   $("#results").html(htmlstring);
